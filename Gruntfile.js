553cb91f1e2744ac1abea4a35d69384f6fd79ea5
Peter Neish <peter.neish@gmail.com>
2015-02-10 03:00:08 +0000

    tweaking Gruntfile copy

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,7 +18,8 @@ module.exports = function(grunt) {
     copy: {
       dist: {
         files: [ {src: 'index.html', dest: 'dist/'},
-                 {src: 'bundle.js', dest: 'dist/'}]
+                 {src: 'bundle.js', dest: 'dist/'},
+                 {src: 'css/**', dest: 'dist/'}]
       }
     }
   });
