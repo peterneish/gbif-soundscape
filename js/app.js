82f2186aa3983d7cddc5d9167c84a7c108d04b87
Peter Neish <peter.neish@gmail.com>
2015-02-18 22:18:19 +1100

    added leaflet map

--- js/app.js
+++ js/app.js
@@ -1,14 +1,28 @@
 var localities = [];
 var active_locality = {};
+var map;
 
-    $.getJSON('./data/sounds.json', function( data){
+map = new L.Map('map');
+
+// create the tile layer with correct attribution
+var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+var osm = new L.TileLayer(osmUrl, {attribution: osmAttrib});		
+
+// start the map in South-East England
+// map.setView(new L.LatLng(0, 0),1);
+map.fitWorld().zoomIn();
+map.addLayer(osm);
+
+
+$.getJSON('./data/sounds.json', function( data){
        
-       $.each( data, function(key, val){
-          localities.push(val);
-          console.log(val);
-       }) 
-       addControls();
-    });
+   $.each( data, function(key, val){
+      localities.push(val);
+      console.log(val);
+   }) 
+   addControls();
+});
     
     
 
